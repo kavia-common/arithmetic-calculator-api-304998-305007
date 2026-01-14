c40b60a1e917d6298d74233ddfb7231b03de9425
Kavia AI <production@kavia.ai>
2026-01-14 17:34:26 +0000

    Checkpoint 1 - cg4cac10a1

---
+++ arithmetic_calculator_api_backend/src/main/java/com/example/demo/arithmeticcalculatorapibackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.arithmeticcalculatorapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class arithmeticcalculatorapibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(arithmeticcalculatorapibackendApplication.class, args);
+	}
+
+}
